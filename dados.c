9313dd38a92c533c7ba223123456d841c587a462
Cau <emaildofakey@gmail.com>
2026-06-29 20:23:26 -0300

    Estrutura os dados de usuario de de livros

---
+++ dados.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_LIVROS 100
+#define MAX_USUARIOS 50
+#define MAX_EMPRESTIMOS 200
+
+typedef struct
+{
+    char titulo[100];
+    char autor[50];
+    int ano;
+    int quantidade;
+    int quantidadeDisponivel;
+    int id;
+} livro;
+typedef struct
+{
+    char nome[50];
+    char email[100];
+    int telefone;
+    int id;
+    livro;
+} usuario;
+
+
+livro books[MAX_LIVROS];
+usuario user[MAX_USUARIOS];
+
+
+
