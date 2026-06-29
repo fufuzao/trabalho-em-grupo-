bdfcfe5c319de05d847244c0a868d8c9483fd2f5
fufuzao <vfuzao@gmail.com>
2026-06-29 20:14:36 -0300

    cadastro e main
    cadastro e main

--- main.c
+++ main.c
@@ -5,6 +5,70 @@
 #define MAX_LIVROS 100
 #define MAX_USUARIOS 50
 #define MAX_EMPRESTIMOS 200
+#define MAX_NOMES 50
+
+void medulo1(){
+    int opcao2,i,j;
+
+   struct main
+   {
+   int Código [50];
+   char Título [50];
+   char Autor [50];
+   char Categoria [50];
+   int Ano [50];
+   int Quantidade [50];
+   int disponível [50];
+    
+   };
+   
+
+
+    do {
+        printf("\n============ BIBLIOTECA ============\n");
+        printf("1 - Cadastrar\n");
+        printf("2 - Listar\n");
+        printf("3 - Buscar\n");
+        printf("4 - Alterar\n");
+        printf("5 - Excluir\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao2);
+    
+        switch(opcao2){
+            
+            
+            case 1:
+
+            break;
+
+
+            case 2:
+                
+           
+            break;
+
+         }
+          
+    }while (opcao2!=0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -41,4 +105,3 @@ int opcao;
           
     }while (opcao!=0);
 }
-
