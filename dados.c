74a9c7cd3821bfd05deada172a6a7e8559fd12f7
Cau <emaildofakey@gmail.com>
2026-06-29 21:36:25 -0300

    adiciona base de menus para os modulos 1 e 2

--- dados.c
+++ dados.c
@@ -6,6 +6,7 @@
 #define MAX_USUARIOS 50
 #define MAX_EMPRESTIMOS 200
 
+// Structs de usuarios e livros
 typedef struct
 {
     char titulo[100];
@@ -26,8 +27,11 @@ typedef struct
 } usuario;
 usuario user[MAX_USUARIOS];
 
+// Variaveis
+int livrosTotais, pessoasTotais;
+
 // Prototipos
-int menuPrincipal();
+void menuPrincipal();
 void menuGerenciarLivros();
 void limpa();
 void limparBufferEntrada();
@@ -40,12 +44,48 @@ int main()
 }
 
 // Funcoes
+void menuPrincipal()
+{
+    printf("\n============ BIBLIOTECA ============\n");
+    printf("1 - Gerenciar Livros\n");
+    printf("2 - Gerenciar Usuarios\n");
+    printf("3 - Emprestimos\n");
+    printf("4 - Relatorios\n");
+    printf("5 - Excluir Livro\n");
+    printf("0 - Voltar ao Menu Principal\n");
+    printf("Escolha: ");
+}
+
+void menuGerenciarLivros()
+{
+    printf("\n============ GERENCIAMENTO DE LIVROS ============\n");
+    printf("1 - Cadastrar Livro\n");
+    printf("2 - Listar Livros\n");
+    printf("3 - Buscar Livro\n");
+    printf("4 - Alterar Livro\n");
+    printf("5 - Excluir Livro\n");
+    printf("0 - Voltar ao Menu Principal\n");
+    printf("Escolha: ");
+}
+
+void menuGerenciarUsuarios()
+{
+    printf("\n============ GERENCIAMENTO DE LIVROS ============\n");
+    printf("1 - Cadastrar novo Usuario\n");
+    printf("2 - Listar Usuarios\n");
+    printf("3 - Buscar Usuario\n");
+    printf("4 - Alterar Cadastro\n");
+    printf("5 - Excluir Cadastro\n");
+    printf("0 - Voltar ao Menu Principal\n");
+    printf("Escolha: ");
+}
 
 void limpa()
 {
     system("cls");
     system("clear");
 }
+
 void limparBufferEntrada()
 {
     int c;
@@ -54,5 +94,4 @@ void limparBufferEntrada()
     {
     }
 }
-void menuGerenciarLivros(){}
-    
+
