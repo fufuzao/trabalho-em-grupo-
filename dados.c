39c5458983ebff95ffacc639d096b063e686545f
fufuzao <vfuzao@gmail.com>
2026-06-30 19:24:29 -0300

    vinicius
    parte escrita 30/06

--- dados.c
+++ dados.c
@@ -6,6 +6,7 @@
 #define MAX_USUARIOS 50
 #define MAX_EMPRESTIMOS 200
 
+// Structs de usuarios e livros
 typedef struct
 {
     char titulo[100];
@@ -26,11 +27,15 @@ typedef struct
 } usuario;
 usuario user[MAX_USUARIOS];
 
+// Variaveis
+int livrosTotais, pessoasTotais;
+
 // Prototipos
-int menuPrincipal();
+void menuPrincipal();
 void menuGerenciarLivros();
 void limpa();
 void limparBufferEntrada();
+void emprestimo();
 
 // Main
 int main()
@@ -40,12 +45,48 @@ int main()
 }
 
 // Funcoes
+void menuPrincipal()
+{
+    printf("\n============ BIBLIOTECA ============\n");
+    printf("1 - Gerenciar Livros\n");
+    printf("2 - Gerenciar Usuarios\n");
+    printf("3 - Emprestimos\n");
+    printf("4 - Relatorios\n");
+    printf("5 - Excluir Livro\n");
+    printf("0 - Voltar ao Menu Principal\n");
+    printf("Escolha: ");
+}
+
+void menuGerenciarLivros()
+{
+    printf("\n============ GERENCIAMENTO DE LIVROS ============\n");
+    printf("1 - Cadastrar Livro\n");
+    printf("2 - Listar Livros\n");
+    printf("3 - Buscar Livro\n");
+    printf("4 - Alterar Livro\n");
+    printf("5 - Excluir Livro\n");
+    printf("0 - Voltar ao Menu Principal\n");
+    printf("Escolha: ");
+}
+
+void menuGerenciarUsuarios()
+{
+    printf("\n============ GERENCIAMENTO DE LIVROS ============\n");
+    printf("1 - Cadastrar novo Usuario\n");
+    printf("2 - Listar Usuarios\n");
+    printf("3 - Buscar Usuario\n");
+    printf("4 - Alterar Cadastro\n");
+    printf("5 - Excluir Cadastro\n");
+    printf("0 - Voltar ao Menu Principal\n");
+    printf("Escolha: ");
+}
 
 void limpa()
 {
     system("cls");
     system("clear");
 }
+
 void limparBufferEntrada()
 {
     int c;
@@ -54,5 +95,65 @@ void limparBufferEntrada()
     {
     }
 }
-void menuGerenciarLivros(){}
+
+
+
+
+
+void estruturaDaMain(){}
+
+
+
+int buscarUsuario(char nome[50]){
+
+for(i = 0;i ==pessoasTotais; i++){
+
+}
+
+
+}
+
+
+
+void emprestimo(){
+
+    printf("\nnome de usuario: ");
+    scanf("%c",user->nome);
     
+
+
+    if(user->nome==1){
+        printf("usuario %c encontrado",user->nome);
+    }
+    else{
+        printf("Usuario nao encontrado");
+    }
+
+
+
+
+
+
+
+
+
+
+    if(books->titulo==1){
+        printf("Livro encontrado");
+
+    }
+    else{
+        printf("Livro nao encontrado");
+    }
+    
+
+
+
+
+
+}
+//Usuário existe?
+//Livro existe?
+//Está disponível?
+//Registrar empréstimo
+//Quantidade disponível--
